794531ddc30ce1f782df6bd0a73963e2d147125c
Charles Guerrero <charles.allfiction@gmail.com>
2021-08-24 18:12:45 +0800

    Changed something at register cuz of react-hook-form update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { Link } from 'react-router-dom';
-import logo from './logo.svg';
 import {TodoList} from './TodoList';
 import {CreateTodo} from './CreateTodo';
 import {EditTodo} from './EditTodo';
