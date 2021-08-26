cab35d0c0ba1000da73073ed3b938a8e3e1abffa
Charles Guerrero <charles.allfiction@gmail.com>
2021-08-26 21:39:07 +0800

    switching

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -1,33 +1,19 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useHistory } from "react-router";
+import React from "react"; 
+import { TodoForm } from "./TodoForm"; 
 
 export const CreateTodo = () => {
-    const {register, handleSubmit} = useForm();
-    const history = useHistory()
 
-    const onSubmit = handleSubmit((data) => {
-        alert(JSON.stringify(data));
-        history.push("/")
+    const onSubmit = ((data) => {
+        alert(JSON.stringify(data)); 
     });
 
 
-    return( 
+    return (
         <div className="container">
             <div className="mt-3">
                 <h3>Create Todo Item</h3>
-                <form onSubmit={onSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="text">Text:</label>
-                        <input className="form-control" ref={register('text')} type="text" id="text"/>
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary">
-                            Create Todo
-                        </button>
-                    </div>  
-                </form>
-            </div>
+                <TodoForm onSubmit={onSubmit}/>
+            </div> 
         </div>
-    );
-};
+        );
+    };
---
+++ src/TodoForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router";
+
+export const TodoForm = ({todo, onSubmit}) => {
+
+    const {register, handleSubmit} = useForm({defaultValues: {text: todo ? todo.text : ""},});
+    const history = useHistory()
+
+
+
+    const submitHandler = handleSubmit((data) => {
+        onSubmit(data)
+        history.push("/");
+    });     
+
+    return( 
+
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <label htmlFor="text">Text:</label>
+                <input className="form-control" ref={register('text')} type="text" id="text"/>
+            </div>
+            <div className="form-group">
+                <button type="submit" className="btn btn-primary">
+                    Create Todo
+                </button>
+            </div>  
+        </form>
+    );
+};
